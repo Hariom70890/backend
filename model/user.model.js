cc3c432b1c947b6708901896d2e91048cbb1b140
Hariom70890 <hariomsunilverma@gmail.com>
2023-07-20 01:08:17 +0530

    done

--- model/user.model.js
+++ model/user.model.js
@@ -12,6 +12,6 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const UserModel = mongoose.model("users", userSchema);
+const UserModel = mongoose.model("user", userSchema);
 
 module.exports = UserModel;
